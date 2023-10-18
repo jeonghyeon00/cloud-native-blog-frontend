067f5c35b7471a719b3ec993e2717db23dbe12e9
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-10-18 13:13:09 +0900

    chore(server): allopen

--- my-blog-server/build.gradle.kts
+++ my-blog-server/build.gradle.kts
@@ -42,3 +42,14 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
